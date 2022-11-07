cf13da36fba741cc0c084f492526e24cb3d4114b
Mischa Rodermond <m.rodermond@nfir.nl>
2022-11-07 12:22:44 +0100

    Removing unused imports

--- BoincWatcher.cs
+++ BoincWatcher.cs
@@ -1,9 +1,4 @@
-﻿using System.Diagnostics;
-using System.Globalization;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
-using System.Timers;
+﻿using System.Timers;
 
 using Timer = System.Timers.Timer;
 
