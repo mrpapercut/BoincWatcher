4d217f07e70e9a5eb6816a0529b5132293f4bdaa
Mischa Rodermond <m.rodermond@nfir.nl>
2022-11-14 15:43:11 +0100

    Updated migration

--- database/migrations/01-initial-setup.sql
+++ database/migrations/01-initial-setup.sql
@@ -4,7 +4,7 @@ USE `dcmanager`;
 --
 -- Host: 127.0.0.1    Database: dcmanager
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE `projects` (
   `name` varchar(45) NOT NULL,
   `project_url` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,11 +40,10 @@ DROP TABLE IF EXISTS `tasks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasks` (
-  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `wu_name` varchar(45) NOT NULL,
   `received` datetime DEFAULT NULL,
-  `deadline` datetime DEFAULT NULL,
+  `report_deadline` datetime DEFAULT NULL,
   `project_id` int DEFAULT NULL,
   `state` int DEFAULT NULL,
   `active_task_state` int DEFAULT NULL,
@@ -57,19 +56,15 @@ CREATE TABLE `tasks` (
   `resources` varchar(45) DEFAULT NULL,
   `suspended_via_gui` tinyint DEFAULT '0',
   `is_fast_dc` tinyint DEFAULT '0',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`name`),
   UNIQUE KEY `name_UNIQUE` (`name`),
   UNIQUE KEY `wu_name_UNIQUE` (`wu_name`),
   KEY `fk_task_project_idx` (`project_id`),
   CONSTRAINT `fk_task_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping routines for database 'dcmanager'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -78,4 +73,4 @@ CREATE TABLE `tasks` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-14 10:11:42
+-- Dump completed on 2022-11-14 14:04:48
